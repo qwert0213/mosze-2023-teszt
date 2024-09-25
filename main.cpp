f83df40acadb838d76f6aa27e94d81d8450676bc
derdavid <david2004212@gmail.com>
2024-09-25 11:29:30 +0200

    fixed

--- main.cpp
+++ main.cpp
@@ -4,22 +4,22 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    //int *b = new int[NELEMENTS];
-    //std::cout << '1-100 ertekek duplazasa'
-    /*for (int i = 0;)
-    {
-        b[i] = i * 2;
-    }*/
+    int* b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa";
     for (int i = 0; i; i++)
     {
-        //std::cout << "Ertek:"
+        b[i] = i * 2;
+    }
+    for (int i = 0; i; i++)
+    {
+        std::cout << "Ertek:";
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    /*for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i]
-    }*/
+        atlag += b[i];
+    }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
     return 0;
